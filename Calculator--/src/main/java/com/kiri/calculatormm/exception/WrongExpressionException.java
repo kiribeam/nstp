74bfd4bcaff5c1f8f5f07b93d1daa77d2358b749
root <root@localhost.localdomain>
2019-07-29 23:41:53 -0400

    -

--- Calculator--/src/main/java/com/kiri/calculatormm/exception/WrongExpressionException.java
+++
@@ -1,8 +0,0 @@
-package com.kiri.calculatormm.exception;
-
-public class WrongExpressionException extends BasicRuntimeException{
-	private static final long serialVersionUID = 1L;
-	public WrongExpressionException(String message) {
-		super("Expression wrong at : " + message);
-	}
-}
--- Calculator--/src/main/java/com/kiri/calculatormm/structure/data/SymbolData.java
+++
@@ -1,5 +0,0 @@
-package com.kiri.calculatormm.structure.data;
-
-public class SymbolData {
-
-}
